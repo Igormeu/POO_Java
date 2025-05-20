dc945a69997b6760a7499b7c39685ec2d5b83b8b
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-05-20 20:42:46 -0300

    Create Array_Products.java

---
+++ Trabalho/Array_Products.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myworkspace.main;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author 202403930111
+ */
+public final class Array_Products{
+    Products[] Array_products;
+    int count;
+    
+    public Array_Products(){
+        Array_products = Products[10];
+        count = 0;
+    }
+    
+}
